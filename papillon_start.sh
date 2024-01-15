e731c9151a89cea3cf9e8f2a00f357d1d8e6c42a
Tryon <68423470+tryon-dev@users.noreply.github.com>
2024-01-15 19:34:26 +0100

    Update papillon_start.sh

--- papillon_start.sh
+++ papillon_start.sh
@@ -3,7 +3,7 @@
 echo "Papillon    ^=^z    EN COURS : Mise à jour du repo api Papillon"
 
 start_api() {
-    kill $(lsof -ti:8000)
+    kill -9 $(fuser -k -n tcp 8000)
     rm -rf papillon-python
     pip install --upgrade pip
     pip uninstall pronotepy -y
